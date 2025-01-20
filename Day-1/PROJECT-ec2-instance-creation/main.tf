701684f7ab495ce4812b9afa6967a79c9d4908c0
Jayakumarkt64 <jayakumarkt64@gmail.com>
2025-01-20 21:20:54 +0530

    changed main.tf

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -1,8 +1,25 @@
-provider "aws" {
-    region = "us-east-1"  # Set your desired AWS region
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
 
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-south-1"
+}
+
+# Create a VPC
 resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
-    instance_type = "t2.micro"
+  ami = "ami-0d2614eafc1b0e4d2"
+  instance_type= "t2.micro"
+  subnet_id= "subnet-01fca5be2131c0465"
+  key_name= "terraform"
+
+  tags={
+   Name= "terraform"
+}
 }
